e350da5bf4225ce253a67c91b5cc4a0aa176ce23
Magcdev <manuel117@gmail.com>
2024-03-13 17:07:59 -0300

    added table

---
+++ frontAspBasic/Models/Games.cs
@@ -0,0 +1,11 @@
+﻿namespace frontAspBasic.Models;
+
+public class Games
+{ 
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+
+}
